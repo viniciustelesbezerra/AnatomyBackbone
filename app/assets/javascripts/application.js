2332d2073dcc90ff003c27a80eadefd2c46b33ef
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-01-19 22:41:59 -0200

    Sending all set

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require jquery
+//= require jquery_ujs
+//= require backbone-rails
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require router
+
+$(function(){ TodoApp.start() });
